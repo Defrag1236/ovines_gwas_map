92623e0744745999f602d3acfb36b07097e1878f
zlobin <defrag12@gmail.com>
2019-12-10 23:52:56 +0700

    Update

--- manipulation_with_plink_files/01_make_bed_reference_for_2nd_48_sheeps.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash 
-
-cp /home/common/projects/ovine_selection/GWAS_on_russian_population_of_ovines/data/plink_files/ped_3m/ped_3m_1_trait.ped /home/common/projects/ovine_selection/ovines_gwas_map/Data/plink_files/plink_for_2nd_48_sheeps.ped
-cp /home/common/projects/ovine_selection/GWAS_on_russian_population_of_ovines/data/plink_files/ped_3m/ped_3m_1_trait.map /home/common/projects/ovine_selection/ovines_gwas_map/Data/plink_files/plink_for_2nd_48_sheeps.map
-
-cd /home/common/projects/ovine_selection/ovines_gwas_map/Data/plink_files
-
-plink --file plink_for_2nd_48_sheeps --extract /home/common/projects/ovine_selection/ovines_gwas_map/Data/Filtered_snp_list_from_reference.txt --make-bed --out plink_for_2nd_48_sheep_reference
--- manipulation_with_plink_files/03_calculate_pca_on_96_sheeps.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash 
-
-cd  /home/common/projects/ovine_selection/ovines_gwas_map/Data/plink_files
-
-plink --bfile plink_96_sheeps_reference_excluded --pca --out pca_96_sheeps
-
-plink --bfile plink_96_sheeps_reference_excluded --maf 0.1 --geno 0.05 --pca --out pca_96_sheeps_filtered
