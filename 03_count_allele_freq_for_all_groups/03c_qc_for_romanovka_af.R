f7440fcbe10971ce388be309a2651339add3a11b
zlobin <defrag12@gmail.com>
2019-12-11 20:53:04 +0700

    Update scripts

--- 03_count_allele_freq_for_all_groups/03c_qc_for_romanovka_af.R
+++ 03_count_allele_freq_for_all_groups/03c_qc_for_romanovka_af.R
@@ -12,18 +12,47 @@ setwd("/home/common/projects/ovine_selection/ovines_gwas_map/results")
 
 romanovka_af <- fread("romanovka_freq.frq", head=T, stringsAsFactors=F)
 
-# compare snps set, doesnt work
+# filter data
 
-reference_clear <- reference[(reference$SNP_name %in% romanovka_af_clear$SNP),]
 
-reference_clear <- reference_clear[!duplicated(reference_clear$SNP_name),]
+
+reference_clear <- reference[!duplicated(reference$SNP_name),]
 reference_clear <- reference_clear[!is.na(reference_clear$SNP_name),]
-
-rownames(reference_clear) <- reference_clear$SNP_name
+reference_clear$eaf <- round(reference_clear$eaf, digits=4)
 
 romanovka_af_clear <- romanovka_af[!duplicated(romanovka_af$SNP),]
 romanovka_af_clear <- romanovka_af_clear[!is.na(romanovka_af_clear$SNP),]
+romanovka_af_clear <- romanovka_af_clear[!grepl(pattern="0", x=romanovka_af_clear$A1),]
+romanovka_af_clear <- romanovka_af_clear[!grepl(pattern="-", x=romanovka_af_clear$A1),]
+romanovka_af_clear <- romanovka_af_clear[!grepl(pattern="-", x=romanovka_af_clear$A2),]
+romanovka_af_clear$MAF <- round(romanovka_af_clear$MAF, digits=4)
 
-rownames(romanovka_af_clear) <- romanovka_af_clear$SNP
+reference_clear <- reference_clear[(reference_clear$SNP_name %in% romanovka_af_clear$SNP),]
 
-reference_clear <- reference_clear[order(rownames(reference_clear), rownames(romanovka_af_clear)),]
+# make romanovka af lexicographical order
+
+alleles <- paste(romanovka_af_clear$A1, romanovka_af_clear$A2, sep="")
+
+romanovka_af_clear <- cbind(romanovka_af_clear, alleles) 
+
+romanovka_af_clear <- romanovka_af_clear[order(romanovka_af_clear$alleles),]
+
+# prepare to plot 
+
+reference_clear <- reference_clear[match(romanovka_af_clear$SNP, reference_clear$SNP_name),]
+
+# make plot
+
+png("romanovka_af_vs_reference.png")
+
+plot(reference_clear$eaf, romanovka_af_clear$MAF)
+
+dev.off()
+
+
+#try to find some mistakes
+
+
+romanovka_af_clear[!(romanovka_af_clear$MAF == reference_clear$eaf),]
+
+reference_clear[grepl(pattern="oar3_OAR26_7891956", x=reference_clear$SNP_name),]
